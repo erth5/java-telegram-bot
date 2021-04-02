4c4ed0cc3e3e2aff5e4be238b1ccedfc501eb75d
Eggo5 <rthem@gmx.de>
2021-04-02 14:07:07 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+// Example taken from https://github.com/rubenlagus/TelegramBotsExample
+public class Main {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            /*
+            telegramBotsApi.registerBot(new ChannelHandlers());
+            telegramBotsApi.registerBot(new DirectionsHandlers());
+            telegramBotsApi.registerBot(new RaeHandlers());
+            telegramBotsApi.registerBot(new WeatherHandlers());
+            telegramBotsApi.registerBot(new TransifexHandlers());
+            telegramBotsApi.registerBot(new FilesHandlers());
+             */
+
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
